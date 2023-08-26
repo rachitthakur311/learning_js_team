dfd1b2490dc4286b44855b3d96ee5e3540d90385
Rachit Rawat <rachitthakur311@gmail.com>
2023-08-26 12:36:41 +0000

    conversion of dtatatypes

---
+++ 01_basics/03_conversionAndOperation.js
@@ -0,0 +1,34 @@
+let score = "rawat"
+
+
+// console.log(typeof score) 
+// console.log(typeof(score)) //method
+
+let valueInNumber = Number(score) 
+// console.log(valueInNumber)
+
+
+
+//NOTES
+//"33" => 33 
+//"33abcd" => NaN
+// true => 1, false => 0
+
+
+
+let isLoggedIn = "rachit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false 
+// "" => false 
+// "rachit" => true
+
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
